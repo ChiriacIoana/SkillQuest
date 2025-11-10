f2a9f17d23c07c644ba0bbeec2162b936905e750
ChiriacIoana <chiriacioana2008@gmail.com>
2025-11-10 23:28:14 +0200

    changed base url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5500',
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5500',
+  baseURL: "https://skillquest-fgym.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken'); 
+  const token = localStorage.getItem("accessToken");
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
