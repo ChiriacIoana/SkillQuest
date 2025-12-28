6f818317595e157cb39aaaed961975476d36ab62
ChiriacIoana <chiriacioana2008@gmail.com>
2025-12-28 18:52:28 +0200

    changed logo for the 10th time

--- src/components/common/dashboard/site-header.tsx
+++ src/components/common/dashboard/site-header.tsx
@@ -13,19 +13,19 @@ export function SiteHeader() {
     <Card className="bg-white dark:bg-gray-800 shadow-lg rounded-xl">
       <CardHeader className="flex flex-row items-center gap-4 p-0">
         <img
-          src="/sq_logo_v2.png"    
+          src="/final_logo.png"    
           alt="SkillQuest Logo"
-          width={60}
-          height={70}
-          className="ml-8"
+          width={64}
+          height={64}
+          className="ml-8 mt-2"
         />
 
         <h1
            className="text-2xl sm:text-4xl lg:text-5xl font-bold tracking-[0.2em] 
               text-transparent bg-clip-text bg-gradient-to-r from-white to-purple-500
-              uppercase select-none ml-4 sm:ml-10 lg:ml-24 "
+              uppercase select-none ml-4 sm:ml-7 lg:ml-15 "
           style={{
-            transform: 'scaleX(1.1)', 
+            transform: 'scaleX(1)', 
           }}
         >
           SkillQuest
